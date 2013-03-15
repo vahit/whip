ca65fb7701a9f142d5cd0199d64a0a3d1fdd6d22
Vahit <vahid.maani@gmail.com>
2013-03-15 18:27:06 +0330

    First Version. That Sould Be Run With DCEWidget.

--- whip.py
+++ whip.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python
+
+##################################
+# Whip Sound App For NOKIA N900. #
+# Just For Fun.                  #
+# Vahid.Maani@gmail.com          #
+##################################
+
+# pyglet and mutagen modules sould be installed.
+import pyglet
+import sys
+import mutagen
+
+pyglet.options['audio'] = ('alsa','silent')
+sound = pyglet.media.load("whip.mp3", streaming=False)
+source = mutagen.File("whip.mp3")
+sound.play()
+pyglet.clock.schedule_once(pyglet.app.exit(), source.info.length)
+pyglet.app.run()
